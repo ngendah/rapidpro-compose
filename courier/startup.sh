3e05542f51d8ed15fe539fffe6f353a8a8473f9e
Ngenda Henry <ngendahk@gmail.com>
2020-12-12 07:35:52 +0000

    Add courier service that enables sending and receiving of SMS

---
+++ courier/startup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+export COURIER_ADDRESS=0.0.0.0
+export COURIER_DB=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgresql/${POSTGRES_DB}?sslmode=disable
+
+RETRIES=20
+STATUS=1
+until wget -q --method=HEAD $BACKEND_URL > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for $BACKEND_URL server, $RETRIES remaining attempts ..."
+  RETRIES=$((RETRIES-=1))
+  sleep 10
+done
+
+wget -q --method=HEAD $BACKEND_URL > /dev/null 2>&1
+STATUS=$?
+if [ "$STATUS" -ne 0 ]; then
+  echo "courier unable to connect to $BACKEND_URL"
+  exit 1
+fi
+
+exec courier
