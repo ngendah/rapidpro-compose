621ca72f664840e3aa20776a6feaddf18ae2bc2c
ngendah <ngendah@pm.me>
2025-07-02 23:05:19 +0300

    Add admin command to help with initial admin-user setup

---
+++ rapidpro/stack/admin.py
@@ -0,0 +1,83 @@
+import pytz
+from django.core.management.base import BaseCommand
+
+from temba.orgs.models import Org, OrgRole, User
+from temba.settings import *
+
+from temba.settings_common import USER_TIME_ZONE, BRANDS, DEFAULT_BRAND
+
+
+class Command(BaseCommand):  # pragma: no cover
+    help = "Utility to manage admin user"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--username', type=str, required=True)
+        parser.add_argument('--email', type=str, required=True)
+        parser.add_argument('--password', type=str, required=True)
+        parser.add_argument('--root', action='store_true')
+
+    def handle(self, *args, **options):
+        name = options['username']
+        email = options['email']
+        password = options['password']
+        is_superuser = options['root']
+        user =  self.create_user(username=name, email=email, password=password, is_root=is_superuser)
+        org = self.create_org(user)
+        self.add_user_to_org(user, org)
+
+    def create_user(self, username:str, email:str, password:str, is_root:bool=False)->User:
+        user = User.objects.filter(username=username).first()
+        if user:
+            self.stdout.write(self.style.NOTICE(f"User {username} already exists, skipping ...")+"\n")
+            return user
+        self.stdout.write(f"Creating user {username} ... ")
+        if is_root:
+            user = User.objects.create_superuser(username, email, password)
+        else:
+            user = User.objects.create_user(username=username, email=email)
+            user.set_password(password)
+        user.save()
+        self.stdout.write(self.style.SUCCESS("OK") + "\n")
+        return user
+
+    def create_org(self, user:User)->Org:
+        brand = self.default_brand
+        if not brand:
+            raise CommandError(f"Brand not found for the default organization {DEFAULT_BRAND}")
+        name = brand['name']
+        slug = brand['slug']
+        org = Org.objects.filter(name=name).first()
+        if not org:
+            self.stdout.write(f"Creating organization {name} ... ")
+            timezone = pytz.timezone(USER_TIME_ZONE)
+            api_url = brand['link']
+            org = Org.objects.create(
+                name=name,
+                timezone=timezone,
+                brand=slug,
+                slug=slug,
+                features=[Org.FEATURE_USERS, Org.FEATURE_NEW_ORGS, Org.FEATURE_CHILD_ORGS],
+                flow_languages=["eng"],
+                created_by=user,
+                modified_by=user,
+            )
+            org.initialize()
+        self.stdout.write(self.style.SUCCESS("OK") + "\n")
+        return org
+
+    def add_user_to_org(self, user:User, org:Org)->Org:
+        self.stdout.write(f"Adding user {user.username} to org {org.name}")
+        if org.has_user(user):
+            self.stdout.write(self.style.NOTICE(f"User {user.username} already belongs to organization {org.name} ... ") + "\n")
+            return org
+        org.add_user(user, OrgRole.ADMINISTRATOR)
+        org.save()
+        self.stdout.write(self.style.SUCCESS("OK") + "\n")
+        return  org
+
+    @property
+    def default_brand(self)->str | None:
+        for brand in BRANDS:
+            if brand['slug']==DEFAULT_BRAND:
+                return brand
+        return None
